85381ce1b66b89a47f065073a3ceb301de1c9c31
rhammad2 <hammadraian25@gmail.com>
2026-04-04 22:42:50 -0500

    Add files via upload

---
+++ tax_calculator_gui.py
@@ -0,0 +1,47 @@
+from breezypythongui import EasyFrame
+
+class TaxCalculator(EasyFrame):
+    """Application window for the tax calculator."""
+
+    def __init__(self):
+        EasyFrame.__init__(self, title = "Tax Calculator")
+
+        self.addLabel(text = "Gross income", row = 0, column = 0)
+        self.incomeField = self.addFloatField(value = 0.0, row = 0, column = 1)
+
+        self.addLabel(text = "Dependents", row = 1, column = 0)
+        self.dependField = self.addIntegerField(value = 0, row = 1, column = 1)
+
+        self.computeButton = self.addButton(text = "Compute",
+                                            row = 2, column = 0,
+                                            columnspan = 2,
+                                            command = self.computeTax)
+
+        self.addLabel(text = "Total tax", row = 3, column = 0)
+        self.taxField = self.addFloatField(value = 0.0, row = 3, column = 1)
+        self.taxField["state"] = "readonly"
+
+    def computeTax(self):
+        TAX_RATE = 0.20
+        STANDARD_DEDUCTION = 10000.0
+        DEPENDENT_DEDUCTION = 3000.0
+
+        grossIncome = self.incomeField.getNumber()
+        numDependents = self.dependField.getNumber()
+
+        taxableIncome = grossIncome - STANDARD_DEDUCTION - \
+                        DEPENDENT_DEDUCTION * numDependents
+        incomeTax = taxableIncome * TAX_RATE
+
+        if incomeTax < 0:
+            incomeTax = 0.0
+
+        self.taxField["state"] = "normal"
+        self.taxField.setNumber(incomeTax)
+        self.taxField["state"] = "readonly"
+
+def main():
+    TaxCalculator().mainloop()
+
+if __name__ == "__main__":
+    main()
